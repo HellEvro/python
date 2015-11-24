99e0a1c3e357fcea29697fe8ba431b475428658e
Luis Iván Cuende <me@luisivan.net>
2015-11-24 15:28:45 +0100

    Dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from distutils.core import setup
 setup(
   name = 'stampery',
   packages = ['stampery'],
-  version = '0.1',
+  version = '0.1.1',
   description = 'Stampery API for Python. Notarize all your data using the blockchain',
-  author = 'Luis Iván Cuende',
+  author = 'Luis Ivan Cuende',
   author_email = 'luis@stampery.com',
   url = 'https://github.com/stampery/python',
   download_url = 'https://github.com/stampery/python/archive/master.zip',
   keywords = ['blockchain', 'stampery', 'notary', 'notarization'],
   classifiers = [],
+  install_requires=[
+    "requests",
+  ],
 )
