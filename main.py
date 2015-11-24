8a6cd914f5d12a80a339f4aaa9693a0b17b3a015
Luis Iván Cuende <me@luisivan.net>
2015-11-24 13:41:16 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import json
+import base64
+import hashlib
+import requests
+
+class Stampery:
+    def __init__(self, apiSecret, beta=False):
+        self.apiSecret = apiSecret
+
+        md5 = hashlib.md5()
+        md5.update(apiSecret)
+        self.clientId = md5.hexdigest()[:15]
+
+        auth = base64.b64encode(self.clientId + ':' + apiSecret)
+        self.headers = {'Authorization': auth}
+
+        if not beta:
+            self.endpoint = 'https://stampery.herokuapp.com/api/v2'
+        else:
+            self.endpoint = 'https://stampery-beta.herokuapp.com/api/v2'
+
+    def hash(self, data):
+        h = hashlib.sha256()
+        h.update(data)
+        return h.hexdigest()
+
+    def stamp(self, data, file=None):
+        if file is None:
+            self.__stampData(data)
+        else:
+            self.__stampFile(data, file)
+
+    def __stampData(self, data):
+        r = requests.post(self.endpoint + '/stamps/', json=data, headers=self.headers)
+        return r.json()
+
+    def __stampFile(self, data, file):
+        files = {'file': ('filsename', file), 'data': (None, json.dumps(data))}
+        r = requests.post(self.endpoint + '/stamps/', files=files, data=data, headers=self.headers)
+        return r.json()
+
+    def get(self, hash):
+        r = requests.get(self.endpoint + '/stamps/' + hash)
+        return r.json()
