bd0053cc26ded9913714e42605a75e3113c7b3fb
Johann Og <kronolynx@gmail.com>
2016-08-04 12:20:34 +0200

    Implement hash function

--- index.py
+++ index.py
@@ -1,18 +1,21 @@
-import msgpackrpc
 import md5
+import hashlib
+import sha3
 
 class Stampery:
-    apiEndpoints = {'prod' : ['api.stampery.com', 4000],
+    __apiEndpoints = {'prod' : ['api.stampery.com', 4000],
                      'beta' : ['api-beta.stampery.com', 4000]}
-    amqpEndpoints = {'prod' : ['young-squirrel.rmq.cloudamqp.com', 5672, 'consumer', '9FBln3UxOgwgLZtYvResNXE7', 'ukgmnhoi'],
+    __amqpEndpoints = {'prod' : ['young-squirrel.rmq.cloudamqp.com', 5672, 'consumer', '9FBln3UxOgwgLZtYvResNXE7', 'ukgmnhoi'],
                      'beta' : ['young-squirrel.rmq.cloudamqp.com', 5672, 'consumer', '9FBln3UxOgwgLZtYvResNXE7', 'beta']}
 
     def __init__(self, secret, branch = 'prod'):
-        self.clientSecret = secret
-        self.clientId = md5.new(secret).hexdigest()
-        self.apiEndpoint = self.apiEndpoints[branch] or self.apiEndpoints['prod']
-        self.amqpEndpoint = self.amqpEndpoints[branch] or self.amqpEndpoints['prod']
+        self.__clientSecret = secret
+        self.__clientId = md5.new(secret).hexdigest()
+        self.__apiEndpoint = self.__apiEndpoints[branch] or self.__apiEndpoints['prod']
+        self.__amqpEndpoint = self.__amqpEndpoints[branch] or self.__amqpEndpoints['prod']
 
     def start(self):
-        print self.apiEndpoint
-        print self.amqpEndpoint
+        print self.hash("hello bitcoin")
+
+    def hash(self, data):
+        return hashlib.sha3_512(data).hexdigest().upper()
